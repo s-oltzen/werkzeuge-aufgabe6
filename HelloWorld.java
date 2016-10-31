c831d9d81ad14ab640b691122207aed1b7c2dbda
Dennis Boldt <boldt@itm.uni-luebeck.de>
2016-10-31 14:34:02 +0100

    Multi-Hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,10 @@ public class HelloWorld {
 	public HelloWorld(String[] args) {
 		if(args.length == 1) {
 			System.out.println(this.hello() + " " + args [0] + "!");
+		} else if(args.length > 1) {
+			for(int i = 0; i < args.length; i++) {
+				System.out.println(this.hello() + " " + args [i] + "!");
+			}
 		} else {
 			System.out.println(this.hello() + " " + this.world() + "!");
 		}
